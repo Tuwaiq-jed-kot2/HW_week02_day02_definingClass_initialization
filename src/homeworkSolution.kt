f87cd3cc4a1a08c770302f50c5afd072c9005c11
shahad197 <shahad.yousef.jed.k2@tuwaiq.edu.sa>
2021-10-18 23:08:41 +0300

    great HW

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,25 @@
 
-
-
-
-
 fun main() {
+    val car = Car("cerato",2017,"white")
+    car.accelerate()
+    car.useBreake()
 
+}
+class Car(val name:String,val manufacturingYear:Int){
+    init {
+        println("$name welcome to our dealerShop")
+    }
+
+    constructor(Tname:String,TmanufacturingYear:Int,color:String): this(name= Tname,manufacturingYear = TmanufacturingYear){
+    println("your car is $color")
+    }
+    fun accelerate()  {
+        println("your car is speeding up")
+
+    }
+
+    fun useBreake(){
+        println("your car is slowing down")
+
+    }
 }
