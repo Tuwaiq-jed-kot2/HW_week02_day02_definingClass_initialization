53d718b980fedc6608af2dfc0b65d0660608efc2
irakan4 <irakan4>
2021-10-18 22:14:37 +0300

    done

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,24 @@
+class Car (var name:String,var manufacturingYear:Int){
 
+    var color:String ="white"
+    constructor(name:String, manufacturingYear:Int,color:String):this(name,manufacturingYear){
+        this.color = color
+    }
+    init {
+        println("welcome to $name car")
+    }
+    fun breake(){
+        println(" $name Breake!!")
+    }
 
+}
 
+fun main(){
+    val mercedes = Car("mercedes",2020)
+    println(mercedes.name)
+    val lexus = Car("lexus",2020,"white")
+    println(lexus.name)
+    println(lexus.color)
 
-
-fun main() {
-
+    lexus.breake()
 }
