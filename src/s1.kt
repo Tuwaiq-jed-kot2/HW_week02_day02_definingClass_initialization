7327b056991fb5c716f113daf9d0f3d0df913fc2
madod <Rawan.hmaid.jed.k2@tuwaiq.edu.sa>
2021-10-18 22:08:18 +0300

    ..

---
+++ src/s1.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+}
+class car(val name:String , val color:String,val manufacturingYear:Int){
+
+
+
+
+
+    fun info(name: String,manufacturingYear: Int){
+        println("p")
+
+    }
+
+}
