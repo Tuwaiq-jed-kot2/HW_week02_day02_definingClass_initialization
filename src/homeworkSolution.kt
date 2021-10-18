f43c908ea40b2314e89ed7f7cf08e0841ed1ea27
yasminbasim <Yasmin.Basim.jed.k2@tuwaiq.edu.sa>
2021-10-18 23:37:55 +0300

    thank you

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,28 @@
+fun main(){
+    val person= car("M","red",1976)
+    person.printInfo()
+
+    val driverSpeed =88
+    if(driverSpeed<=75)
+
+        if (driverSpeed in 75..100){
+            println("the car speed is accelerate")
+        }else if (driverSpeed in 58..74){
+            println("the car speed is fine")
+        }else println("the car is going to break")
 
 
 
+}
+class car(val name:String,val color:String,val manufacturingYear:Int ){
 
+    init { println("welcome")
 
-fun main() {
+    }
+    fun printInfo(){
+        ("hello your car name $name and the color $color amd the manufacturingYear $manufacturingYear ")
+
+    }
+
 
 }
