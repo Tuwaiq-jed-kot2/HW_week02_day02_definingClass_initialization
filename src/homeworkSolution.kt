aef11f8d0acc515f59ecdb546c3226c911804bf7
Qussai <qussai.Saeed.jed.k2@tuwaiq.edu.sa>
2021-10-18 12:39:02 -0400

    choose me

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,34 @@
-
-
-
-
-
 fun main() {
+    val bmw = car("BMW", "black", 2021)
+    val ford = car("Ford", "white", 1999)
+    accelerate()
+    Break()
+}
 
+class car(val Name: String, val manufacturingYear: Int) {
+
+
+    constructor(Cname: String, Ccolor: String, CmanufacturingYear: Int) : this(Name = Cname,manufacturingYear = CmanufacturingYear
+    ) {
+    }
+
+    init {
+        println("welcome your car is $Name")
+    }
+}
+
+
+fun accelerate() {
+    println("type your speed by kilo")
+    val S = readLine()?.toInt()
+    println("type the time by minutes")
+    val A = readLine()?.toInt()
+    val m = S!! / A!!
+    println("the car acceleration is $m sec per hour")
+
+}
+
+
+fun Break() {
+    println("the breaks doesn't work ")
 }
