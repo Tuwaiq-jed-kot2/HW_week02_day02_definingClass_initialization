274835b52ce0820821dafc7ad1de65f2438f7d0d
AbdulghaniAlhazmi <Abdulghani.Hassan.jed.k2@tuwaiq.edu.sa>
2021-10-18 16:36:07 +0300

    solved

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,28 @@
+class Car(var name : String  , var  carYear : Int){
 
+    var carColour : String =""
 
+    constructor(carName: String, cManYear: Int, colour: String) : this(name = carName, carYear = cManYear) {
+        carColour = colour
+    }
 
+    init {
+        println("$name Registered")
+    }
+
+    fun accelerate(){
+        println("$name Accelerate !!")
+    }
+    fun breake(){
+        println("$name Breake !!")
+    }
+
+}
 
 
 fun main() {
-
+    val car1 = Car("Car1",2020)
+    val car2 = Car("Car2",2018,"Red")
+    car1.accelerate()
+    car2.breake()
 }
