c8cad755b788cd78ae98cb42ef403e17c710bf16
ASUS <EshraqAlmalki>
2021-10-18 20:29:41 +0300

    thanks A lot!!

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,28 @@
-
-
-
-
-
 fun main() {
+    val C =Car ("BMW" ,2021)
+    var name="BMW"
+}
+
+class Car (var name:String , var manufacturingYear:Int){
+
+
+
+    init {
+        println("HI , Here Is Your $name")
+    }
+
+
+    constructor(Sname:String , SmanufacturingYear:Int , color:String) :
+            this(name = Sname , manufacturingYear= SmanufacturingYear)
+
+
+    fun accelerate(){
+    }
+
+    fun useBreak(){
+    }
+
 
 }
+
+
