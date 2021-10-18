c60670349963e188a4f5a7bd28ddb1b8a5c030cb
roya allam <Roya.Adel.jed.k2@tuwaiq.edu.sa>
2021-10-18 22:08:42 +0300

    thank you

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,25 @@
-
-
-
-
-
 fun main() {
+    val carOne=Car("MG",2020)
+    val carTew=Car("FEW",2021,"Red")
+    carOne.accelerate()
+    carTew.useBreake()
 
+
+}
+class Car(val name:String,val manufacuringYear:Int) {
+    var color: String = ""
+    constructor(nameC: String, manufacuringYearC: Int, colorC: String) : this(
+        name=nameC,
+        manufacuringYear=manufacuringYearC) {
+        color=colorC
+    }
+    init {
+        println("WELCOMING THE CAR : $name ")
+    }
+    fun accelerate(){
+        println("The car accelerate")
+    }
+    fun useBreake(){
+        println("The car use break")
+    }
 }
