463eba256c01563c8651ab282096fa3b2b5d85eb
Bashayr yahya al zahrani <Bashayr.Yahya.jed.k2@tuwaiq.edu.sa>
2021-10-19 02:31:53 +0300

    i tried to do the best

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,44 @@
 
 
-
-
-
 fun main() {
+    val ca1 = Car()
+    println("write name of car")
+    ca1.nameC = (readLine())
+    println("enter the color ")
+    ca1.color =(readLine())
+    println("add your name : ")
+    var name:(String?)= readLine()
+    println("Welcome $name ")
+    ca1.manuFacturingYear=2012
+    println("Car fractured in 2017")
+    ca1.carSpeed = 0
+    println("your car speed ${ca1.carSpeed}")
+    ca1.accelerateandOfCar()
+    ca1.accelerateandOfCar()
+    ca1.useBrake()
+
+
 
 }
+
+
+class Car {
+
+    var nameC: (String?) = ""
+    var color: (String?) = ""
+    var manuFacturingYear: (Int) = 0
+    var carSpeed: (Int) = 0
+
+    fun accelerateandOfCar (){
+        if (carSpeed!==0){
+            println("\" your Car  in Accelerate mood \"")
+        }
+    }
+
+
+
+    fun useBrake (){if (carSpeed==0)
+        println("the car in brake mood")
+    }
+}
+
