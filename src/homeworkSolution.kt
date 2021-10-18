6d8c99dba5106c1ac608c9435eba09940b47ffbb
AsrarAlbishri <Asrar.Mohammad.jed.k2@tuwaiq.edu.sa>
2021-10-18 23:14:55 +0300

    thank you

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,36 @@
-
-
-
-
-
 fun main() {
+
+    val car1 = Car("camry", 2017)
+
+    car1.accelerate()
+    car1.useBreak()
+
+    val car2 = Car("lexus", 2021, "white")
+
+    car2.accelerate()
+    car2.useBreak()
+
+}
+
+class Car(val carName: String, val carManufacturingYear: Int) {
+
+    var carColor: String = ""
+
+    constructor(cName: String, cManufacturingYear: Int, color: String) :
+            this(carName = cName, carManufacturingYear = cManufacturingYear) {
+        carColor = color
+    }
+
+    init {
+        println("Welcome to $carName")
+    }
+
+    fun accelerate() {
+        println("car accelration")
+    }
+
+    fun useBreak() {
+        println("$carName break is working")
+    }
 
 }
