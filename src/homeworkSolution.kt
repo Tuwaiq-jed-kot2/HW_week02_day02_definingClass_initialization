c429eca68bdada0118d536b80c25728cffa72cea
HumaidALKhathami <humaid.ali.jed.k2@tuwaiq.edu.sa>
2021-10-18 18:10:43 +0300

    great homework

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,48 @@
+import kotlin.reflect.jvm.internal.impl.resolve.constants.IntegerValueConstant
 
+class Car (val name:String?, manufacturingYear:Int? ){
+    init {
+        println("welcome $name owner ")
+    }
+
+    constructor (tName: String?,tManufacturingYear: Int?,color:String?) : this(name = tName  , manufacturingYear = tManufacturingYear){
+
+    }
+
+    fun accelerate (){
+        println("the car is accelerating")
+    }
+
+    fun useBrake (){
+        println("the car is decelerating")
+    }
+
+}
 
 
 
 
 fun main() {
+    println("please type in your car name")
+    val name = readLine()
+
+    println("please type in your car color")
+    val color = readLine()
+
+    try {
+        println("please type in your car manufacturing year")
+        val manufacturingYear = readLine()?.toInt()
+
+        val car = Car (name,manufacturingYear , color)
+
+        car.accelerate()
+
+        car.useBrake()
+
+    }catch (e : NumberFormatException){
+
+        println("you entered invalid manufacturing year")
+
+    }
 
 }
