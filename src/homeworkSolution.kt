a2002e91160450dd153f88c15d91defea03ca00d
yasminbasim <Yasmin.Basim.jed.k2@tuwaiq.edu.sa>
2021-10-19 00:06:55 +0300

    thank you

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,28 +1,26 @@
 fun main(){
     val person= car("M","red",1976)
     person.printInfo()
-
-    val driverSpeed =88
-    if(driverSpeed<=75)
-
-        if (driverSpeed in 75..100){
-            println("the car speed is accelerate")
-        }else if (driverSpeed in 58..74){
-            println("the car speed is fine")
-        }else println("the car is going to break")
-
-
+    person.accelerate("speed")
 
 }
-class car(val name:String,val color:String,val manufacturingYear:Int ){
+class car(val name:String,val color:String,val manufacturingYear:Int ) {
 
-    init { println("welcome")
+
+    init {
+        println("wellcom")
+    }
+    val speed = mutableListOf<String>()
+
+    fun printInfo() {
+        println("hello your car name $name and the color $color amd the manufacturingYear $manufacturingYear ")
 
     }
-    fun printInfo(){
-        ("hello your car name $name and the color $color amd the manufacturingYear $manufacturingYear ")
 
+    fun accelerate(car: String) {
+        if (speed.contains(name)) {
+            println("need to slow")
+        } else (speed.add(name))
+        println("your speed fine")
     }
-
-
 }
