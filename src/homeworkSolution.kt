a35e0c10a781b7ca39e386d34a590d196b65c02e
bayanalharthy <Bayan.Mastour.jed.k2@tuwaiq.edu.sa>
2021-10-19 07:39:40 +0300

    hi

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,28 @@
 
 
-
-
-
 fun main() {
 
+val ferrari = Car("ferrari" , "back" , 2021 )
+
+
 }
+
+class Car(val name : String , val color: String , val manufacturing: Int){
+   constructor (name:String,manufacturing: Int, color: String ):this (name:String,manufacturing:Int){
+
+    }
+
+init {
+   println("welcome $name")
+}
+
+
+fun accelerate(){
+println( )
+
+}
+fun use breake (){
+    println( )
+}
+
+
