64a38a9562e541b7574d33dec6789b8bfb09d383
yasminbasim <Yasmin.Basim.jed.k2@tuwaiq.edu.sa>
2021-10-19 08:44:14 +0300

    thank you

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -2,7 +2,7 @@ fun main(){
     val person= car("M","red",1976)
     person.printInfo()
     person.accelerate("speed")
-
+    person.break()
 }
 class car(val name:String,val color:String,val manufacturingYear:Int ) {
 
@@ -10,17 +10,27 @@ class car(val name:String,val color:String,val manufacturingYear:Int ) {
     init {
         println("wellcom")
     }
+
     val speed = mutableListOf<String>()
+    var carS =0
 
     fun printInfo() {
         println("hello your car name $name and the color $color amd the manufacturingYear $manufacturingYear ")
 
     }
+    fun break(car:String){
+
+    }
+
+
 
     fun accelerate(car: String) {
         if (speed.contains(name)) {
             println("need to slow")
         } else (speed.add(name))
+        carS++
         println("your speed fine")
     }
+}
+}
 }
