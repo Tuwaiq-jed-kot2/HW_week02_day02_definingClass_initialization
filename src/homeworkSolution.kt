da8dd4a6400ac27d4892c51e4dcf1c383a9dda0b
ranaowaidhi <ranaowaidhi@gmail.com>
2021-10-19 05:57:06 +0300

    physics is my game? I hope so ◘-◘

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,37 @@
-
-
-
-
-
 fun main() {
+
+    val car1 = Car("Honda",2020)
+    val carAcc =car1.accelerate(10,60,2)
+    println("The car accelerate $carAcc per second")
+
+    car1.brake(0)
+
+}
+
+class Car(val name:String, val manufacturingYear:Int){
+
+    lateinit var carColor:String
+
+    init {
+        println("Welcome، The car name is $name, Manufacturing Year is $manufacturingYear.")
+    }
+
+    constructor(cName:String , cManufacturingYear:Int , color:String) :
+            this(name = cName,manufacturingYear = cManufacturingYear) {
+        carColor = color
+    }
+
+    fun accelerate(v1:Int, v2:Int, t:Int): Int {
+        //The difference between the two speeds divided by the time in seconds
+        return (v2-v1)/t
+    }
+
+    fun brake(speed:Int){
+        if (speed == 0){
+            println("The car is not moving")
+        } else {
+            println("The car is moving")
+        }
+    }
 
 }
