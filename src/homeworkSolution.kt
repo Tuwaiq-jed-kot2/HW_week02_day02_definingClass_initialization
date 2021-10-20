8c994bbcc34be3cc4c65ed7997ffb9fe837195da
Sara <sarabakheet>
2021-10-20 20:01:49 +0300

    I had a problem with the assignment, and I contacted you about it, I apologize again

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,44 @@
 
 
 
-
-
 fun main() {
 
+    val Car1 = Car("Audi", "white", 2022)
+    Car1.Properties()
+    Car1.accelerate()
 }
+class  Car(var nameOfCar: String?, var colorOfCar: String?)
+{ constructor(cName: String?, cColor: String?, manufacturingYear: Int?)
+        :this(name = cName, color = cColor)
+
+    init {
+
+        println("hello $nameOfCar")
+
+    }
+
+    fun Properties() {
+        println("car of name : ${this.nameOfCar} colors  is: ${this.colorOfCar} ")
+
+    }
+
+    fun accelerate() {
+        TODO("Not yet implemented")
+    }
+
+
+}
+fun accelerate() {
+
+    println("The car accelerate")
+
+}
+
+
+fun useBreake() {
+    println("The car in break mood")
+}
+
+
+
+
